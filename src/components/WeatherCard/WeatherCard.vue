8d647a58760c8f806606b226e49ecb75814beda8
Igor Shapovalov <kizore5@gmail.com>
2023-08-01 19:20:59 +1000

    add computed methods to weather card

--- src/components/WeatherCard/WeatherCard.vue
+++ src/components/WeatherCard/WeatherCard.vue
@@ -4,16 +4,21 @@
       <h1 class="title city">{{ city.name }},{{ city.country }}</h1>
     </header>
     <div class="main-info">
-      <img src="../../../public/02d@4x.png" alt="" width="100" />
+      <img
+        :src="`https://openweathermap.org/img/wn/${city.main.icon}@2x.png`"
+        :alt="city.main.main"
+        width="100"
+      />
       <div class="main-info__temperature">{{ city.main.temp }} °C</div>
     </div>
     <em class="short-details">
-      Feels like {{ city.main.feels }} °C. Broken clouds. Light breeze
+      Feels like {{ city.main.feels }} °C. {{ formatClouds }}.
+      {{ windPhase }}
     </em>
     <div class="long-details">
       <div class="long-details__prop">
         <WindIcon />
-        {{ city.detail.wind.speed }}m/s SSE
+        {{ city.detail.wind.speed }}m/s {{ windDirection }}
       </div>
       <div class="long-details__prop">
         <PressureIcon />
@@ -22,32 +27,21 @@
       <div class="long-details__prop">
         Humidity: {{ city.detail.humidity }}%
       </div>
-      <div class="long-details__prop">Dew point: {{}}%</div>
-      <div class="long-details__prop">
-        Visibility: {{ city.detail.visibility / 1000 }}km
-      </div>
+      <div class="long-details__prop">Dew point: {{ dewPoint }}%</div>
+      <div class="long-details__prop">Visibility: {{ formatVisibility }}km</div>
     </div>
   </li>
 </template>
 
 <script lang="ts">
-/*
-Расчитать точку росы
-Направление ветра
-путь до картинок
- облака
-
-
-
-
-
-
-*/
-
 import PressureIcon from '../ui/icons/PressureIcon.vue'
 import WindIcon from '../ui/icons/WindIcon.vue'
 import { CityWeather } from '../../types'
 import { defineComponent, PropType } from 'vue'
+import { getWindDirection } from '../../helpers/getWindDirection'
+import { getWindPhrase } from '../../helpers/getWindPhrase'
+import { getCloudsPhrase } from '../../helpers/getCloudsPhrase'
+import { getDewPoint } from '../../helpers/getDewPoint'
 
 export default defineComponent({
   components: { WindIcon, PressureIcon },
@@ -55,6 +49,29 @@ export default defineComponent({
     city: {
       type: Object as PropType<CityWeather>,
       required: true,
+    },
+  },
+  computed: {
+    weatherShortPhase() {
+      return `Feels like ${this.city.main.feels} °C. {{ formatClouds }}.
+      {{ windPhase }}`
+    },
+
+    windDirection() {
+      return getWindDirection(this.city.detail.wind.speed)
+    },
+    windPhase() {
+      return getWindPhrase(this.city.detail.wind.speed)
+    },
+    formatClouds() {
+      const first = this.city.main.description[0].toUpperCase()
+      return `${first}${this.city.main.description.slice(1)}`
+    },
+    dewPoint() {
+      return getDewPoint(this.city.main.temp, this.city.detail.humidity)
+    },
+    formatVisibility() {
+      return Math.floor(this.city.detail.visibility / 1000).toFixed(1)
     },
   },
 })
